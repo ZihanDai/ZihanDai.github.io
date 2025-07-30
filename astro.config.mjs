95ac4192f2da58e58bef4369243fe6c20f66e3b1
ZihanDai <zihan_d@outlook.com>
2025-07-31 00:24:26 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  output: 'static',
+  site: 'https://zihandai.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
